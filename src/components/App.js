7bc368fb60dac1431e8258996e5bf63e860cf464
freebeego <freebeego@yandex.ru>
2021-06-17 02:29:23 +0300

    feat: Logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logon from './Logo';
+
+const TotalBalances = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  list-style: unset;
+`;
+
+const TotalBalance = styled.li`
+
+`;
+
+const Type = styled.span`
+  font-size: 18px;
+  line-height: 21px;
+  color: #fff;
+  opacity: 0.5;
+`;
+
+const Amount = styled.span`
+
+`;
 
 function App() {
   return (
-    <>asd</>
+    <>
+      <Logon />
+      <TotalBalances>
+        <TotalBalance>
+
+        </TotalBalance>
+      </TotalBalances>
+    </>
   );
 }
 
---
+++ src/components/Logo/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { ReactComponent as LogoSVG } from '../../images/logo.svg';
+
+const Logo = styled(LogoSVG)`
+  width: 188px;
+  height: 57px;
+`;
+
+export default Logo;
