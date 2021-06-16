0221384fcf17b49d27544f57ae5ecc69ea68d466
freebeego <freebeego@yandex.ru>
2021-06-16 16:46:07 +0300

    feat: add fonts Roboto and Rubik

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
+import './vendor/fonts.css';
 import './index.css';
 import App from './components/App';
 import { store } from './store/store';
